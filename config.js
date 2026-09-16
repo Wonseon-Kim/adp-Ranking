f8360362a4758300034ad64aff20e715c9fdb1f7
Wonseon-Kim <ulteraboy@gmail.com>
2026-09-16 13:28:22 +0900

    Initial website upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Apps Script 배포 후 나온 웹 앱 URL을 여기에 붙여넣으세요.
+// 예: https://script.google.com/macros/s/AKfycb.../exec
+const API_URL = "https://script.google.com/macros/s/AKfycbwVh8tyjsYZIq7Kv39q3f9g0-sRfHJnFtc9dnF1RnOPjiFhrmOwmfVbI3L2j8YUcOC-fw/exec";
